506e2f31ed81cec024a553f53f9add48e4550460
Tonny <95202310+Tonnfu22@users.noreply.github.com>
2024-09-01 17:31:09 +0300

    Update coingeko.py

--- coingeko.py
+++ coingeko.py
@@ -1,21 +1,7 @@
 import requests
 
-COINGECKO_API_URL = "https://api.coingecko.com/api/v3/simple/price"
-SUPPORTED_COINS = {
-    'bitcoin': 'BTC',
-    'litecoin': 'LTC',
-    'ethereum': 'ETH',
-    'monero': 'XMR',
-    'tether': 'USDT'
-}
-
-def get_crypto_price(coin):
-    if coin not in SUPPORTED_COINS:
-        return None
-    params = {
-        'ids': coin,
-        'vs_currencies': 'usd'
-    }
-    response = requests.get(COINGECKO_API_URL, params=params)
+def get_crypto_price(coin_id):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+    response = requests.get(url)
     data = response.json()
-    return data[coin]['usd'] if coin in data else None
+    return data.get(coin_id, {}).get('usd', None)
